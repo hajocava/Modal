3ce905d2eae8786b0dac68a1a7c21f79591b0188
Haziel Castillo <haziel.castillo@truehome.com.mx>
2022-03-27 23:02:08 -0600

    Clean code

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -2,6 +2,7 @@ import { Body } from './Body';
 import { Footer } from './Footer';
 import { Header } from './Header';
 import { ModalProps } from './interfaces';
+import closeIcon from './closeIcon.svg';
 import './styles.sass'
 
 export const Modal = ({
@@ -21,30 +22,43 @@ export const Modal = ({
     blurEffect = false,
 }: ModalProps) => {
 
-    const closeModalWhenCLicOutside = (e: any) => {
+    const closeModalWhenClickOutside = (e: any) => {
         if (e.target.className?.length > 0 && e.target.className.split(" ").includes(classCloseModal)) {
             setShow(false);
         }
     }
-    
+
     return (
         <div
-            onClick={closeModalWhenCLicOutside }
+            onClick={closeModalWhenClickOutside}
             className={`modal ${blurEffect ? 'blur' : ''}`}
-            style={{ 
+            style={{
                 display: show ? 'block' : 'none',
                 backgroundColor
             }}
         >
             <div
-                className={`modal-content ${size} ${classNames} ${animated ? 'animated' : ''} ${animStyle} ${position} ${fullScreen ? "full-screen":""}`}
+                className={`
+                    modal-content 
+                    ${size} 
+                    ${classNames} 
+                    ${animated ? 'animated' : ''} 
+                    ${animStyle} 
+                    ${position} 
+                    ${fullScreen ? "full-screen" : ""}
+                `}
                 style={{ ...style }}
             >
                 {
                     showCloseButton && (
-                        <div className="close" onClick={() => setShow(false)}>
-                            <svg viewBox="0 0 24 24" width="24" height="24"><g><path d="M2.47 2.47a.75.75 0 011.06 0L12 10.939l8.47-8.47a.75.75 0 01.976-.072l.084.073a.75.75 0 010 1.06L13.061 12l8.47 8.47a.75.75 0 01.072.976l-.073.084a.75.75 0 01-1.06 0L12 13.061l-8.47 8.47a.75.75 0 01-.976.072l-.084-.073a.75.75 0 010-1.06L10.939 12l-8.47-8.47a.75.75 0 01-.072-.976z" fill="#262E66" fillRule="evenodd"></path></g></svg>
-                        </div>
+                        <img
+                            onClick={() => setShow(false)}
+                            className="close"
+                            src={closeIcon}
+                            alt="close modal"
+                            width="24px"
+                            height="24px"
+                        />
                     )
                 }
                 {children}
